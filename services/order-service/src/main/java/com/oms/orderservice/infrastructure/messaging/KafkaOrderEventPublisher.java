44bb5301fdfbfef6dbcf7505e0947b315cb69bf5
print-ramcharan <ramcharanpolabathina@gmail.com>
2026-01-02 18:34:01 +0530

    feat(order): publish OrderCreatedEvent to Kafka

---
+++ services/order-service/src/main/java/com/oms/orderservice/infrastructure/messaging/KafkaOrderEventPublisher.java
@@ -0,0 +1,24 @@
+package com.oms.orderservice.infrastructure.messaging;
+
+import com.oms.orderservice.domain.event.OrderCreatedEvent;
+import com.oms.orderservice.domain.event.OrderEventPublisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+@RequiredArgsConstructor
+public class KafkaOrderEventPublisher implements OrderEventPublisher {
+
+    private final KafkaTemplate<String, OrderCreatedEvent> kafkaTemplate;
+
+    private static final String TOPIC = "order.created";
+
+
+    @Override
+    public void publish(OrderCreatedEvent event) {
+        kafkaTemplate.send(TOPIC, event.orderId().toString(), event);
+    }
+}
