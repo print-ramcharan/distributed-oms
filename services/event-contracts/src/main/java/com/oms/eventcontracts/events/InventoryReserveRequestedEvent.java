8c00a1ff8269fbd8e5d376005f55bb5909522bc6
print-ramcharan <ramcharanpolabathina@gmail.com>
2026-01-06 01:33:55 +0530

    Event contracts: add InventoryReserveRequestedEvent

---
+++ services/event-contracts/src/main/java/com/oms/eventcontracts/events/InventoryReserveRequestedEvent.java
@@ -0,0 +1,58 @@
+package com.oms.eventcontracts.events;
+
+import java.time.Instant;
+
+public class InventoryReserveRequestedEvent {
+
+    private String orderId;
+    private String productId;
+    private int quantity;
+    private Instant requestedAt;
+
+    // Required for Jackson
+    public InventoryReserveRequestedEvent() {}
+
+    public InventoryReserveRequestedEvent(
+            String orderId,
+            String productId,
+            int quantity,
+            Instant requestedAt
+    ) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.requestedAt = requestedAt;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Instant getRequestedAt() {
+        return requestedAt;
+    }
+
+    public void setRequestedAt(Instant requestedAt) {
+        this.requestedAt = requestedAt;
+    }
+}
