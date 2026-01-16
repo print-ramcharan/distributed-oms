f64c8b03bca8d5641506bd3beece0fdef5e719d0
print-ramcharan <ramcharanpolabathina@gmail.com>
2026-01-16 16:28:00 +0530

    refactor(saga): drive order lifecycle via AdvanceOrderProgressCommand

--- services/saga-orchestrator/src/main/java/com/oms/sagaorchestrator/saga/messaging/PaymentFailedListener.java
+++ services/saga-orchestrator/src/main/java/com/oms/sagaorchestrator/saga/messaging/PaymentFailedListener.java
@@ -1,17 +1,22 @@
 package com.oms.sagaorchestrator.saga.messaging;
 
+import com.oms.eventcontracts.commands.AdvanceOrderProgressCommand;
+import com.oms.eventcontracts.enums.OrderProgress;
 import com.oms.eventcontracts.events.PaymentFailedEvent;
 import com.oms.sagaorchestrator.saga.domain.OrderSaga;
 import com.oms.sagaorchestrator.saga.domain.SagaState;
 import com.oms.sagaorchestrator.saga.repository.OrderSagaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class PaymentFailedListener {
     private final OrderSagaRepository sagaRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
 
     @KafkaListener(topics = "payment.failed", groupId = "saga-orchestrator"
 //            , containerFactory = "genericKafkaListenerContainerFactory"
@@ -22,6 +27,16 @@ public class PaymentFailedListener {
         if(saga.getState() != SagaState.PAYMENT_REQUESTED){
             return;
         }
+
+        kafkaTemplate.send(
+                "order.command.advance-progress",
+                String.valueOf(event.getOrderId()),
+                new AdvanceOrderProgressCommand(
+                        event.getOrderId(),
+                        OrderProgress.ORDER_FAILED
+                )
+
+        );
 
         saga.markPaymentFailed();
         sagaRepository.save(saga);
